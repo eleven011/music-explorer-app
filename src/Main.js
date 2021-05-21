e15dc9e4fa7b9be7cba90d0e7c231d99f837d3e7
eleven011 <vkillian@pdx.edu>
2021-05-21 08:51:40 -0700

    oops, forgot to save merge changes

--- src/Main.js
+++ src/Main.js
@@ -4,13 +4,8 @@ import {
   NavLink,
   HashRouter
 } from "react-router-dom";
-<<<<<<< HEAD
 import Search from "./Home";
 import Visuals from "./Visuals";
-=======
-import Home from "./Home";
-import Stuff from "./Stuff";
->>>>>>> spotifyAPI
 import Contact from "./Contact"; 
 import Recommendations from "./Recommendations";
 
@@ -27,13 +22,8 @@ class Main extends Component {
                         <li><NavLink to="/recommendations">Recs</NavLink></li>
                     </ul>
                 <div className="content">
-<<<<<<< HEAD
                     <Route exact path="/" component={Search}/>
                     <Route path="/visuals" component={Visuals}/>
-=======
-                    <Route exact path="/" component={Home}/>
-                    <Route path="/stuff" component={Stuff}/>
->>>>>>> spotifyAPI
                     <Route path="/contact" component={Contact}/>
                     <Route path="/recommendations" component={Recommendations}/>
                 </div>
