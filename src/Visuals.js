7881b2c095352dbdfc06e0435feb1f870d1cf789
nickiwahlers111 <61523696+nickiwahlers111@users.noreply.github.com>
2021-05-20 21:09:48 -0700

    reverted to workng visuals

--- src/Visuals.js
+++ src/Visuals.js
@@ -3,33 +3,33 @@ import Force from "./Force";
 import Search from "./Recommendations";
 
 // dummy JSON object **** this is where a function is run
-// const data = {
-//   name: "Lady Gaga ",
-//   children: [
-//     {
-//       name: "Katy Perry ",
-//       // additional info if needed about Katy Perry
-//       children: [
-//         {
-//           name: "P!nk ",
-//           // additional info if needed about Pink
-//         },
-//         {
-//           name: "Ke$ha",
-//         }
-//       ]
-//     },
-//     {
-//       name: "Rihanna ",
-//     },
-//     {
-//       name: "Beyonce ",
-//     },
-//   ]
-// };
+const data = {
+  name: "Lady Gaga ",
+  children: [
+    {
+      name: "Katy Perry ",
+      // additional info if needed about Katy Perry
+      children: [
+        {
+          name: "P!nk ",
+          // additional info if needed about Pink
+        },
+        {
+          name: "Ke$ha",
+        }
+      ]
+    },
+    {
+      name: "Rihanna ",
+    },
+    {
+      name: "Beyonce ",
+    },
+  ]
+};
 
-function Visuals(data) {
-  console.log("Passed data: ", data);
+function Visuals() {
+  // console.log("Passed data: ", data);
 return (
   <React.Fragment>
     <h2> Test Force Physics</h2>
