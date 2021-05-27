a75a9d7f70fe96130138d169bd0f5cf0a4915b97
nickiwahlers111 <61523696+nickiwahlers111@users.noreply.github.com>
2021-05-27 12:44:44 -0700

    hate these changes hope they dont push

--- src/Recommendations.js
+++ src/Recommendations.js
@@ -19,9 +19,9 @@ function Search() {
   }
 
   return (
-    <div className="container">
-      <div className="search-circle">
-        <label htmlFor="artist-search">
+    <div className="card">
+      <div className="search">
+      <label htmlFor="artist-search">
           <input
             type="text"
             id="artist-search"
@@ -30,6 +30,7 @@ function Search() {
           />
           <input className="btn btn-primary" type="submit" value="search" onClick= {handleOnClick} />
         </label>
+        <br></br>
         <label htmlFor="num-recs"> 
             <input
               id="num-recs"
@@ -39,8 +40,33 @@ function Search() {
               placeholder="# of recs"
             />
         </label>
-      </div>
+        </div>
     </div>
+
+
+
+    // <div className="container">
+    //   <div className="search-circle">
+    //     <label htmlFor="artist-search">
+    //       <input
+    //         type="text"
+    //         id="artist-search"
+    //         placeholder="Search for an artist..."
+    //         onKeyDown={handleOnEnter}
+    //       />
+    //       <input className="btn btn-primary" type="submit" value="search" onClick= {handleOnClick} />
+    //     </label>
+        // <label htmlFor="num-recs"> 
+        //     <input
+        //       id="num-recs"
+        //       type="number"
+        //       min="1" 
+        //       max="10"
+        //       placeholder="# of recs"
+        //     />
+        // </label>
+    //   </div>
+    // </div>
 
   );
 
